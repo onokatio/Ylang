6e152703655a3196a7f51ac52b08d7d9d8455086
onokatio <onokatio@protonmail.com>
2019-07-16 14:25:55 +0900

    show detail

--- vm.js
+++ vm.js
@@ -1,10 +1,11 @@
 const compile = (code) => {
 	opcode_list = code.split(' ');
-	console.log("-----[code]----- \n" + opcode_list);
-	if( code.match(/要するに俺が言いたいのは (.*) ってことだな！$/) !== null ){
+	console.log("---------- \n");
+	console.log("[code] " + opcode_list);
+	if( code.match(/^要するに俺が言いたいのは (.*) ってことだな！$/) !== null ){
 
 		console.log("[mode] echo");
-		console.log("var=" + code.split(' ')[0]);
+		console.log("string=" + code.split(' ')[1]);
 
 	}else if( code.match(/が知りたい！$/) ){
 
@@ -13,6 +14,8 @@ const compile = (code) => {
 
 	}else if( code.match(/は (.*) だな！$/) ){
 		console.log("[mode] var");
+		console.log("var=" + code.split(' ')[0]);
+		console.log("expr=" + code.match(/は (.*) だな！$/)[1]);
 	}else{
 		console.log("[mode] other");
 	}
