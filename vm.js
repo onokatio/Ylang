42cd57119504f383273f131fb5d57381ba962af0
Restyled.io <commits@restyled.io>
2019-12-10 05:38:40 +0000

    Restyled by prettier

--- vm.js
+++ vm.js
@@ -1,111 +1,107 @@
-const print_result =
-    text => {
-      document.getElementById("result").value = text
-      console.log(text)
-    }
+const print_result = text => {
+  document.getElementById("result").value = text;
+  console.log(text);
+};
 
-const compile =
-    (codes) => {
-      var memory = {};
-      opcode_list = codes.split('\n');
-      // console.log("---------- \n");
-      // console.log("[code] " + opcode_list);
-      opcode_list.forEach((code, index) => {
-        if (code.match(
-                /^要するに俺が言いたいのは 「(.*)」 ってことだな！$/) !==
-            null) {
+const compile = codes => {
+  var memory = {};
+  opcode_list = codes.split("\n");
+  // console.log("---------- \n");
+  // console.log("[code] " + opcode_list);
+  opcode_list.forEach((code, index) => {
+    if (
+      code.match(/^要するに俺が言いたいのは 「(.*)」 ってことだな！$/) !== null
+    ) {
+      // console.log("[mode] echo");
+      // console.log("string=" + code.split(' ')[1]);
+      print_result(
+        code.match(/^要するに俺が言いたいのは 「(.*)」 ってことだな！$/)[1]
+      );
+    } else if (
+      code.match(/^要するに俺が言いたいのは (.*) ってことだな！$/) !== null
+    ) {
+      print_var = code.match(
+        /^要するに俺が言いたいのは (.*) ってことだな！$/
+      )[1];
+      print_result(memory[print_var]);
+    } else if (code.match(/が知りたい！$/)) {
+      memory[code.split(" ")[0]] = prompt("入力してください。");
+    } else if (code.match(/は (.*) だな！$/)) {
+      ans_var = code.split(" ")[0];
+      expr = code.match(/は (.*) だな！$/)[1];
+      arg1 = expr.match(/(.*) と (.*) の(.*)$/)[1];
+      if (isNaN(arg1)) {
+        arg1 = memory[arg1];
+      }
+      arg2 = expr.match(/(.*) と (.*) の(.*)$/)[2];
+      if (isNaN(arg2)) {
+        arg2 = memory[arg2];
+      }
+      wasa = expr.match(/(.*) と (.*) の(.*)$/)[3];
+      if (wasa === "和") {
+        ans = parseInt(arg1) + parseInt(arg2);
+      } else if (wasa === "差") {
+        ans = parseInt(arg1) - parseInt(arg2);
+      } else if (wasa === "積") {
+        ans = parseInt(arg1) * parseInt(arg2);
+      } else if (wasa === "除") {
+        ans = parseInt(arg1) / parseInt(arg2);
+      } else if (wasa === "アンド") {
+        ans = parseInt(arg1) & parseInt(arg2);
+      } else if (wasa === "オア") {
+        ans = parseInt(arg1) | parseInt(arg2);
+      }
+      memory[ans_var] = ans;
+    } else if (code.match(/もし (.*) なら$/)) {
+      expr = code.match(/もし (.*) なら$/)[1];
+      if (expr.match(/(.*) が (.*) (.*)$/)) {
+        arg1 = expr.match(/(.*) が (.*) (.*)$/)[1];
+        if (isNaN(arg1)) {
+          arg1 = mamory[arg1];
+        }
+        arg2 = expr.match(/(.*) が (.*) (.*)$/)[2];
+        if (isNaN(arg2)) {
+          arg2 = mamory[arg1];
+        }
+        comp_oper = expr.match(/(.*) が (.*) (.*)$/)[3];
+        if (comp_oper === "と等しい") {
+          if (arg1 === arg2) {
+            ans = true;
+            // console.log("===")
+          } else if (arg1 !== arg2) {
+            ans = false;
+            // console.log("!==")
+          }
+        } else if (comp_oper === "より大きい") {
+          if (arg1 > arg2) {
+            ans = true;
+            // console.log(">")
+          } else if (arg1 < arg2) {
+            ans = false;
+            // console.log("<")
+          }
+        } else if (comp_oper === "より小さい") {
+          if (arg1 < arg2) {
+            ans = true;
+            // console.log("<")
+          } else if (arg1 > arg2) {
+            ans = false;
+            // console.log(">")
+          }
+        }
+      }
 
-          // console.log("[mode] echo");
-          // console.log("string=" + code.split(' ')[1]);
-          print_result(code.match(
-              /^要するに俺が言いたいのは 「(.*)」 ってことだな！$/)
-                           [1]);
-
-        } else if (
-            code.match(
-                /^要するに俺が言いたいのは (.*) ってことだな！$/) !==
-            null) {
-          print_var = code.match(
-              /^要するに俺が言いたいのは (.*) ってことだな！$/)[1];
-          print_result(memory[print_var])
-        } else if (code.match(/が知りたい！$/)) {
-          memory[code.split(' ')[0]] = prompt("入力してください。");
-        } else if (code.match(/は (.*) だな！$/)) {
-          ans_var = code.split(' ')[0];
-          expr = code.match(/は (.*) だな！$/)[1];
-          arg1 = expr.match(/(.*) と (.*) の(.*)$/)[1];
-          if (isNaN(arg1)) {
-            arg1 = memory[arg1];
-          }
-          arg2 = expr.match(/(.*) と (.*) の(.*)$/)[2];
-          if (isNaN(arg2)) {
-            arg2 = memory[arg2];
-          }
-          wasa = expr.match(/(.*) と (.*) の(.*)$/)[3];
-          if (wasa === "和") {
-            ans = parseInt(arg1) + parseInt(arg2);
-          } else if (wasa === "差") {
-            ans = parseInt(arg1) - parseInt(arg2);
-          } else if (wasa === "積") {
-            ans = parseInt(arg1) * parseInt(arg2);
-          } else if (wasa === "除") {
-            ans = parseInt(arg1) / parseInt(arg2);
-          } else if (wasa === "アンド") {
-            ans = parseInt(arg1) & parseInt(arg2);
-          } else if (wasa === "オア") {
-            ans = parseInt(arg1) | parseInt(arg2);
-          }
-          memory[ans_var] = ans;
-        } else if (code.match(/もし (.*) なら$/)) {
-          expr = code.match(/もし (.*) なら$/)[1];
-          if (expr.match(/(.*) が (.*) (.*)$/)) {
-            arg1 = expr.match(/(.*) が (.*) (.*)$/)[1];
-            if (isNaN(arg1)) {
-              arg1 = mamory[arg1];
-            }
-            arg2 = expr.match(/(.*) が (.*) (.*)$/)[2];
-            if (isNaN(arg2)) {
-              arg2 = mamory[arg1];
-            }
-            comp_oper = expr.match(/(.*) が (.*) (.*)$/)[3];
-            if (comp_oper === "と等しい") {
-              if (arg1 === arg2) {
-                ans = true;
-                // console.log("===")
-              } else if (arg1 !== arg2) {
-                ans = false;
-                // console.log("!==")
-              }
-            } else if (comp_oper === "より大きい") {
-              if (arg1 > arg2) {
-                ans = true;
-                // console.log(">")
-              } else if (arg1 < arg2) {
-                ans = false;
-                // console.log("<")
-              }
-            } else if (comp_oper === "より小さい") {
-              if (arg1 < arg2) {
-                ans = true;
-                // console.log("<")
-              } else if (arg1 > arg2) {
-                ans = false;
-                // console.log(">")
-              }
-            }
-          }
-
-          /*
+      /*
           let index_copy = index
           for( opcode_list[index_copy] !== '{' ) index_copy++;
           compile(opcode_list.slice(index,index_copy))
           index = index_copy+1;
           */
-        } else {
-          print_result("[mode] other");
-        }
-      });
+    } else {
+      print_result("[mode] other");
     }
+  });
+};
 
 /*
 sourcecode =
