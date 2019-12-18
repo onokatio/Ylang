e0f32ab0c14aea47974abbb6f355d8694a705811
onokatio <onokatio@gmail.com>
2019-12-19 04:07:51 +0900

    fix alert

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import { Alert } from 'react-bootstrap'
+import { AlertDismissible } from './components/AlertDismissible.jsx'
 
 import hljs from 'highlight.js'
 import 'highlight.js/styles/solarized-dark.css'
@@ -23,7 +24,7 @@ const run_compile = (e) => {
 	result.value = ''
 	const code = document.getElementById("code").value;
 	compile(code,0,{})
-	ReactDOM.render(<Alert variant='primary' dismissible>Compile finished.</Alert>, document.getElementById("alertlist"))
+	ReactDOM.render(<Alert variant='primary'>Compile finished.</Alert>, document.getElementById("alertlist"))
 }
 
 const button = document.getElementById("compileButton")
