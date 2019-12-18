f58e2af7ce4079b0f7eb245f5a2a77a983642598
onokatio <onokatio@gmail.com>
2019-12-19 00:40:40 +0900

    use react.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+};
