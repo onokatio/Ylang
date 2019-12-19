e3235eac79fba6e5720e3e671e4a8913823f70f8
onokatio <onokatio@gmail.com>
2019-12-19 09:06:59 +0900

    add key

--- src/components/SampleCodes.jsx
+++ src/components/SampleCodes.jsx
@@ -4,8 +4,8 @@ import SampleCodeCard from './SampleCodeCard.jsx'
 
 export default class SampleCodes extends React.Component {
 	render() {
-		const CardList = this.props.samples.map( sample => {
-			return (<div><SampleCodeCard title={sample.title} description={sample.description} sourcecode={sample.sourcecode}/></div>)
+		const CardList = this.props.samples.map( (sample,index) => {
+			return (<div key={index}><SampleCodeCard title={sample.title} description={sample.description} sourcecode={sample.sourcecode}/></div>)
 		})
 
 		return (<div> {CardList} </div>)
